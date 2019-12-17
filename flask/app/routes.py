81fac0b1a5f185b41a04e7d4faf008e35a8534c4
George-633 <112063126@qq.com>
2019-12-17 17:02:13 +0800

    add secret key

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,6 +1,19 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello"
+    user = {'username':'duke'}
+    posts = [
+        {
+            'author': {'username': '刘'},
+            'body': '这是模板模块中的循环例子～1'
+
+        },
+        {
+            'author': {'username': '忠强'},
+            'body': '这是模板模块中的循环例子～2'
+        }
+    ]
+    return render_template('index.html', title='My', user=user, posts=posts)
