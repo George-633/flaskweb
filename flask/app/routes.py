19a1dacbf919dde9901b13f927132b041027c839
George-633 <112063126@qq.com>
2019-12-18 14:57:54 +0800

    add login restriction

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,10 +1,13 @@
 from app import app
 from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username':'duke'}
     posts = [
@@ -20,12 +23,23 @@ def index():
     ]
     return render_template('index.html', title='My', user=user, posts=posts)
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('username:{}, remember me:{}'.format(
-            form.username.data, form.remember_me.data)
-        )
-        return redirect('./index')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('undefinded User Name or Password!!!')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
     return render_template('login.html', title='login', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
